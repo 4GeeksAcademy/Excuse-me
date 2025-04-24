5fb75adc576f250e14483adb0c9cb1abbbf76aa0
Jonathan <jonyoner@gmail.com>
2025-04-24 11:11:12 +0000

    ACABADO+BOTÓN

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'my computer'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
+
+
+function getRandom(anyArray) {
+    return anyArray [Math.floor(Math.random() * anyArray.length)];
+  }
+
+window.onload = function () {
+    console.log("Hello Rigo from the console!");
+    document.querySelector('#excuse').innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when)
 };
+
